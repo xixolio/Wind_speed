1a8caef661e802deb7422121f81015a8e8432668
xixolio <ignacio.araya.11@alumnos.usm.cl>
2018-06-15 14:23:29 -0400

    modified qsub to add differenet experiments

--- qsub.py
+++ qsub.py
@@ -10,20 +10,36 @@ import subprocess
 import sys
 
 
-#model = sys.argv[1]
-model = "simple_LSTM"
+model = sys.argv[1]
+
+experiment = int(sys.argv[2])
+#model = "simple_LSTM"
 
 if model == "simple_LSTM":
     
-    layers = ['[5]', '[10]', '[15]', '[20]']
-    
-    lag = [12, 34, 36]
-    
-    time_steps = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,\
-                  18, 19, 20]
-    
-    epochs = [1, 5]
-    
+    if experiment == 1:
+        
+        layers = ['[5]', '[10]', '[15]', '[20]']
+        
+        lag = [12, 24, 36]
+        
+        time_steps = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,\
+                      18, 19, 20]
+        
+        epochs = [1, 5]
+        
+        
+        
+    if experiment == 2:
+        
+        layers = ['[5]']
+        
+        lag = [12]
+        
+        time_steps = [5]
+        
+        epochs = [1]
+        
     combs = product(layers, lag, time_steps, epochs)
     
     for c in combs:
@@ -36,9 +52,9 @@ if model == "simple_LSTM":
                 
                 string += str(element) + ','
             
-            string = 'simple_LSTM.py /user/i/iaraya/CIARP/Wind_speed/data/ \
+            string = 'simple_LSTM /user/i/iaraya/CIARP/Wind_speed/data/ \
                     no_mvs_d05a.csv ' + string
-            #print(string)
+            
             subprocess.call(["qsub","main.sh","-F",string])
             
     
