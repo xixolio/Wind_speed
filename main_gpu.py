83e6893942b2bf89a0d05be160857acc78c6c6de
xixolio <ignacio.araya.11@alumnos.usm.cl>
2018-06-21 17:33:44 -0400

    started steuff for gpu usage

---
+++ main_gpu.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 10 15:07:18 2018
+
+@author: iaaraya
+"""
+import os
+#os.environ["MKL_THREADING_LAYER"] = "GNU"
+import sys
+sys.path.append('/user/i/iaraya/CIARP/Wind_speed/data/')
+sys.path.append('/user/i/iaraya/CIARP/Wind_speed/model_and_functions/')
+#sys.path.append('C:/Users/iaaraya/Documents/CIARP/Wind_speed/data/')
+#sys.path.append('C:/Users/iaaraya/Documents/CIARP/Wind_speed/model_and_functions/')
+
+sys.path.append('/home/iaraya/Wind_speed/data/')
+sys.path.append('/home/iaraya/Wind_speed/model_and_functions/')
+
+from data_processing import get_data
+import simple_LSTM as sLSTM
+import hierarchical_LSTM as hLSTM
+import persistence
+
+import numpy as np
+
+
+if __name__ == "__main__":
+    
+    model = sys.argv[1]
+        
+    path = sys.argv[2]
+        
+    file_name = sys.argv[3]
+        
+    if model == "simple_LSTM":
+      
+        runs = 5
+        
+        sets = 10
+           
+        layers, lag, time_steps, epochs, l2, learning_rate = sLSTM.get_params(4)
+    
+        params = [layers, lag, time_steps, epochs, l2, learning_rate]
+        
+        training_inputs, testing_inputs, training_outputs, testing_outputs,\
+        vmins, vmaxs = get_data(path, file_name, time_steps, lag)
+            
+        mae = np.zeros((sets, runs))
+        mape = np.zeros((sets, runs))
+        mse = np.zeros((sets, runs))
+        
+        for i in range(sets):
+            
+            X = training_inputs[i]
+            X_ts = testing_inputs[i]
+            y = training_outputs[i]
+            y_ts = testing_outputs[i]
+            
+            for j in range(runs):
+                
+                model = sLSTM.model(layers, lag, time_steps, l2, learning_rate)
+                
+                mae[i,j], mape[i,j], mse[i,j], model = sLSTM.train_and_test(model, time_steps, lag, \
+                                                      epochs, vmins[i], vmaxs[i],     \
+                                                      X, y, X_ts, y_ts)
+                
+                model_name = "simple_LSTM_test_set_" + str(i) + "_run_" + str(j) +\
+                '_'.join(str(x) for x in params)
+                #model.save("/user/i/iaraya/CIARP/Wind_speed/models/" + model_name + ".h5")
+               
+        path = "/user/i/iaraya/CIARP/Wind_speed/results/"
+        write_file_name = "simple_LSTM_test_" + file_name[:-4] + ".txt"
+                
+        sLSTM.write_results(path, write_file_name, params, mae, mape, mse)
+        
+        
+    elif model == "hierarchical_LSTM":
+      
+        runs = 5
+        
+        sets = 10
+           
+        lags, time_steps, dense_nodes, lstm_nodes, processed_scales, \
+        epochs, l2 = hLSTM.get_params(4)
+    
+        params = [lags, time_steps, dense_nodes, lstm_nodes, processed_scales,\
+                   epochs, l2]
+        
+        training_inputs_sets = []
+        testing_inputs_sets = []
+        
+        for i in range(lags):
+            
+            training_inputs, testing_inputs, training_outputs, testing_outputs,\
+            vmins, vmaxs = get_data(path, file_name, time_steps[i], lags[i])
+            
+            training_inputs_sets.append(training_inputs)
+            testing_inputs_sets.append(testing_inputs)
+            
+
+            
+        mae = np.zeros((sets, runs))
+        mape = np.zeros((sets, runs))
+        mse = np.zeros((sets, runs))
+        
+        for i in range(sets):
+            
+            min_data_len = 10000000
+            
+            for j in range(lags):
+                
+                if len(training_inputs_sets[j][i]) < min_data_len:
+                    
+                    min_data_len = len(training_inputs_sets[j][i])
+            
+            X = []
+            X_ts = []
+                        
+            for j in range(lags):
+                
+                 X.append(training_inputs_sets[j][i][-min_data_len:])
+                 X_ts.append(testing_inputs_sets[j][i])
+    
+            y = training_outputs[i][-min_data_len:]
+            y_ts = testing_outputs[i]
+            
+            for j in range(runs):
+                
+                model = hLSTM.model(lags, time_steps, processed_scales, \
+                                    dense_nodes, lstm_nodes, l2)
+                
+                mae[i,j], mape[i,j], mse[i,j], model = hLSTM.train_and_test(model, time_steps, lags, \
+                                                      epochs, vmins[i], vmaxs[i],     \
+                                                      X, y, X_ts, y_ts)
+                
+                model_name = "hierarchical_LSTM_set_" + str(i) + "_run_" + str(j) +\
+                '_'.join(str(x) for x in params)
+                #model.save("/user/i/iaraya/CIARP/Wind_speed/models/" + model_name + ".h5")
+               
+        path = "/user/i/iaraya/CIARP/Wind_speed/results/"
+        write_file_name = "hierarchical_LSTM_" + file_name[:-4] + ".txt"
+                
+        hLSTM.write_results(path, write_file_name, params, mae, mape, mse)
+        
+        
+    elif model == "persistence":
+        
+        training_inputs, testing_inputs, training_outputs, testing_outputs,\
+        vmins, vmaxs = get_data(path, file_name, time_steps, lag)
+        
+        mae = np.zeros((sets))
+        mape = np.zeros((sets))
+        mse = np.zeros((sets))
+        
+        for i in range(sets):
+            
+            X = training_inputs[i]
+            X_ts = testing_inputs[i]
+            y = training_outputs[i]
+            y_ts = testing_outputs[i]
+            
+            model = sLSTM.model(layers, lag, time_steps, l2, learning_rate)
+            
+            mae[i], mape[i], mse[i] = persistence.train_and_test(vmins[i], vmaxs[i], y, y_ts)
+            
+            model_name = "persistence_" + str(i) + "_run"
+            #model.save("/user/i/iaraya/CIARP/Wind_speed/models/" + model_name + ".h5")
+           
+        path = "results/"
+        write_file_name = "persistence_" + file_name[:-4] + ".txt"
+                
+        persistence.write_results(path, write_file_name, mae, mape, mse)
+        
+       
--- read_results.py
+++ read_results.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 21 16:25:42 2018
+
+@author: iaaraya
+"""
 
 import numpy as np    
 import sys
 
 
-file_name = sys.argv[1]
-my_file = "simple_LSTM_test_" + file_name[:-4] + ".txt"
-data = []
+# Simple_LSTM
 
+file_name = "no_mvs_d05a.csv"
+my_file = "correctedsimple_LSTM_test_" + file_name[:-4] + ".txt"
+data_mae = []
+data_mse = []
 
 for i in range(10):
 
@@ -16,17 +23,60 @@ for i in range(10):
     lines = f.readlines()
     f.close()
     
-    lines = lines[1]
+    data_mae_set = []
+    data_mse_set = []
     
-    data = lines.split("[")[1:]
-    
-    corrected_data = []
-    
-    for data_line in data:
+    for line in lines:
         
-        corrected_data.append( "[" + data_line )
+        data = line.split(' ')
+        data_mae_set.append(float(data[-6]))
+        data_mse_set.append(float(data[-5]))
         
-     
-    #print(lines)
+    data_mae.append(data_mae_set)
+    data_mse.append(data_mse_set)
+    
+data_mae = np.array(data_mae)
+data_mse = np.array(data_mse)
+
+mean_simple_LSTM_mae = np.mean(data_mae,0)
+mean_simple_LSTM_mse = np.mean(data_mse,0)
+#%%
+
+# persistence
+
+file_name = "no_mvs_d05a.csv"
+my_file = "persistence_" + file_name[:-4] + ".txt"
+data_mae = []
+data_mse = []
+
+for i in range(10):
+
+    f = open("results/" + str(i) + my_file)
+    
+    lines = f.readlines()
+    f.close()
+    
+    data_mae_set = []
+    data_mse_set = []
+    
+    for line in lines:
+        
+        data = line.split(' ')
+        data_mae_set.append(float(data[-5]))
+        data_mse_set.append(float(data[-4]))
+        
+    data_mae.append(data_mae_set)
+    data_mse.append(data_mse_set)
+    
+data_mae = np.array(data_mae)
+data_mse = np.array(data_mse)
+
+mean_persistence_mae = np.mean(data_mae,0)
+mean_persistence_mse = np.mean(data_mse,0)
+        
+    
+        
+    
+
 
     
