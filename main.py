a69fce1b107e2261bb1dfbce5a7d47a5ba4e24f0
xixolio <ignacio.araya.11@alumnos.usm.cl>
2019-03-09 14:21:27 -0300

    asdads

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import sys
 
 import tensorflow as tf
 
-os.environ['CUDA_VISIBLE_DEVICES']='0' # gpu='0' o gpu='1'
+os.environ['CUDA_VISIBLE_DEVICES']='1' # gpu='0' o gpu='1'
 ###################################
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True#Utiliza la memoria que necesita de manera dinamica, puede ser o no en bloque.
