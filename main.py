e31dce0ba93a8f45ebe66279846a503598219873
xixolio <ignacio.araya.11@alumnos.usm.cl>
2019-03-15 09:27:26 -0300

    fixed test

--- main.py
+++ main.py
@@ -154,6 +154,10 @@ if __name__ == "__main__":
             y_val = validation_outputs[i]
             y_ts = testing_outputs[i]
             
+            if experiment == 'test':
+                X = np.concatenate((X,X_val),axis=0)
+                y = np.concatenate((y,y_val[:,0]),axis=0)
+            
             for j in range(runs):
                 
                 if model == 'LSTM_Ms':
@@ -189,13 +193,12 @@ if __name__ == "__main__":
                     write_file_name = str(model) + '_' + file_name[:-4] + "set_"+str(i)+".txt"
                     
                 elif experiment == 'test':
-                    X = np.concatenate((X,X_val),axis=0)
-                    y = np.concatenate((y,y_val[:,0]),axis=0)
+
                     mae[i,j], mse[i,j],h_mae[i,j,:],h_mse[i,j,:], epoch = trf.train(mod, max_input_values, 1, \
                                                           epochs, vmins[i], vmaxs[i],     \
                                                           X, y, copy.deepcopy(X_ts), copy.deepcopy(y_ts),  batch_size = batch_size, \
                                                           shuffle = shuffle,experiment = experiment)
-                    write_file_name = str(model) + '_test2_' + file_name[:-4] + "set_"+str(i)+".txt"
+                    write_file_name = str(model) + '_test3_' + file_name[:-4] + "set_"+str(i)+".txt"
             
             wr.write_result(results_path, write_file_name, params, mae[i], mse[i],h_mae[i],h_mse[i],epoch)
             
